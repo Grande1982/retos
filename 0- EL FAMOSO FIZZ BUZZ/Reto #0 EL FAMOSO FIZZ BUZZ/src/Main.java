0c7f7c47126fb4b6ce90a50c7891e2ea97700d7d
cesar grande <cgrande@cic.es>
2023-09-21 23:03:58 +0200

    primer commit

---
+++ 0- EL FAMOSO FIZZ BUZZ/Reto #0 EL FAMOSO FIZZ BUZZ/src/Main.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author cgrande
+ */
+
+/*
+ * Escribe un programa que muestre por consola (con un print) los
+ * números de 1 a 100 (ambos incluidos y con un salto de línea entre
+ * cada impresión), sustituyendo los siguientes:
+ * - Múltiplos de 3 por la palabra "fizz".
+ * - Múltiplos de 5 por la palabra "buzz".
+ * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        for (int i= 0; i<101; i++){
+            
+            System.out.println("Numero: "+i);
+        }
+    }
+    
+}
