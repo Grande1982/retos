593f9d0732e35eb7ecee6fce0793a5f04d83ca93
cesar grande <cgrande@cic.es>
2023-10-03 23:02:06 +0200

    añadimos reto 6

---
+++ 6 - Piedra, Papel, Tijera, Lagarto, Spock/Reto #6 Piedra, Papel, Tijera, Lagarto, Spock/src/Main.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author cgrande
+ */
+
+/*
+ * Crea un programa que calcule quien gana más partidas al piedra,
+ * papel, tijera, lagarto, spock.
+ * - El resultado puede ser: "Player 1", "Player 2", "Tie" (empate)
+ * - La función recibe un listado que contiene pares, representando cada jugada.
+ * - El par puede contener combinaciones de "🗿" (piedra), "📄" (papel),
+ *   "✂️" (tijera), "🦎" (lagarto) o "🖖" (spock).
+ * - Ejemplo. Entrada: [("🗿","✂️"), ("✂️","🗿"), ("📄","✂️")]. Resultado: "Player 2".
+ * - Debes buscar información sobre cómo se juega con estas 5 posibilidades.
+ */
+
+
+/*
+Las tijeras cortan el papel, el papel envuelve la piedra, la piedra aplasta al 
+lagarto, el lagarto envenena a Spock, Spock aplasta las tijeras, las tijeras 
+decapitan al lagarto, el lagarto devora el papel, el papel desaprueba a Spock, 
+Spock desintegra la piedra y, como siempre, la piedra aplasta las tijeras.
+*/
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
