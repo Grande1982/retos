8ab2edea25c4601b862f3c470a8a2cbb5702bbf3
cgrande1982 <cgrande1982@gmail.com>
2023-12-12 00:22:18 +0100

    haciendo reto 9

--- 9 - Heterograma, isograma y pangrama/Reto_9 Heterograma_isograma_pangrama/src/Main.java
+++ 9 - Heterograma, isograma y pangrama/Reto_9 Heterograma_isograma_pangrama/src/Main.java
@@ -13,6 +13,9 @@
 
 
 import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
@@ -21,27 +24,39 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.println("Introduces una cadena de texto");
         String cadena = sc.nextLine();
-        heterograma(cadena);
-        isograma(cadena);
+        boolean hem = heterograma(cadena);
+        if (hem) {
+            System.out.println("No es Heterograma");
+        } else {
+            System.out.println("Es Heterograma");
+        }
+        boolean iso = isograma(cadena);
+
+        if (iso) {
+            System.out.println("Es isograma");
+        } else {
+            System.out.println("No es isograma");
+        }
+
         pangrama(cadena);
 
     }
 
 
-    public static void heterograma(String cadena) {
+    public static boolean heterograma(String cadena) {
         char caracter1, caracter2;
         boolean noHeterograma = false;
         for (int i = 0; i < cadena.length(); i++) {
             caracter1 = cadena.charAt(i);
-            System.out.println("cogemos la letra: " + caracter1);
+            //System.out.println("cogemos la letra: " + caracter1);
 
             for (int j = 0; j < cadena.length(); j++) {
                 noHeterograma = false;
                 if (i != j) {
                     caracter2 = cadena.charAt(j);
-                    System.out.println("comparamos la letra: " + caracter1 + " con " + caracter2);
+                    //System.out.println("comparamos la letra: " + caracter1 + " con " + caracter2);
                     if (Character.compare(caracter1, caracter2) == 0) {
-                        System.out.println("No es Heterograma");
+
                         noHeterograma = true;
                         break;
                     }
@@ -55,13 +70,95 @@ public class Main {
 
         }
 
+        return noHeterograma;
+
     }
 
-    public static void isograma(String cadena) {
+    public static boolean isograma(String cadena) {
+
+        ArrayList<Character> contenedorLetras = new ArrayList<Character>();
+        char caracter1, caracter2;
+        int contadorInicio = 0, contadorFinal = 0;
+        HashMap<Character, Integer> contadorTotal = new HashMap<Character, Integer>();
+
+        for (int i = 0; i < cadena.length(); i++) {
+            caracter1 = cadena.charAt(i);
+            //System.out.println("cogemos la letra: " + caracter1);
+            contenedorLetras.add(caracter1);
+        }
+
+        for (int j = 0; j < contenedorLetras.size(); j++) {
+            caracter1 = contenedorLetras.get(j);
+
+
+            for (int k = 0; k < contenedorLetras.size(); k++) {
+                caracter2 = contenedorLetras.get(k);
+                if (Character.compare(caracter1, caracter2) == 0) {
+
+                    contadorFinal++;
+                }
+
+            }
+            contadorTotal.put(caracter1, contadorFinal);
+            contadorInicio = contadorFinal;
+            contadorFinal = 0;
+
+        }
+
+
+        boolean eso = false;
+        // Obtener las claves y valores utilizando un bucle for-each y el método entrySet()
+        for (Map.Entry<Character, Integer> entry : contadorTotal.entrySet()) {
+
+            Character clave = entry.getKey();
+            Integer valor = entry.getValue();
+            //System.out.println("Clave: " + clave + ", Valor: " + valor);
+            if (contadorInicio != valor) {
+                eso = false;
+            } else {
+                eso = true;
+            }
+        }
+
+
+        //System.out.println(contadorTotal);
+        return eso;
+
 
     }
 
     public static void pangrama(String cadena) {
 
+        int contadorInicio = 0, contadorFinal = 0;
+        ArrayList<Character> contenedorLetras = new ArrayList<Character>();
+        char[] abecedario = {
+                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+        };
+        char caracter1, caracter2;
+        for (int i = 0; i < cadena.length(); i++) {
+            caracter1 = cadena.charAt(i);
+            //System.out.println("cogemos la letra: " + caracter1);
+            contenedorLetras.add(caracter1);
+        }
+
+
+        for (int j = 0; j < contenedorLetras.size(); j++) {
+            caracter1 = contenedorLetras.get(j);
+
+            for (int k = 0; k < contenedorLetras.size(); k++) {
+                caracter2 = abecedario[k];
+                if (Character.compare(caracter1, caracter2) != 0) {
+
+                    contadorFinal += 1;
+                }else{
+                    contadorFinal = 1;
+                }
+
+            }
+            System.out.println(contadorFinal);
+        }
     }
+
+
 }
